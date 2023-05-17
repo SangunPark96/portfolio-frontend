d19a07e53846eca4d0e9a8ee060500fdadbc8804
SangUn Park <Sangunpark1@gmail.com>
2023-05-17 10:47:01 -0400

    testing css animations

--- portfolio-app/src/Pages/Home.js
+++ portfolio-app/src/Pages/Home.js
@@ -4,8 +4,8 @@ function Home() {
     return (
       <div className="home">
         <h2>Welcome</h2>
-        <h3>This is an on-going project i am working on to post my projects and interests. I plan on expanding it further as i continue to develop my skills.</h3>
-        
+        <h3>Hi, I'm SangUn Park. A passionate Front-end React Developer based in New York City.</h3>
+        <h2>This is an on-going project I am working on to post my projects and interests. I plan on expanding it further as I continue to develop my skills.</h2>
       </div>
     );
   }
---
+++ portfolio-app/src/Pages/ProjectIndex.js
@@ -0,0 +1,11 @@
+
+export default function ProjectIndex() {
+  
+
+
+    return(
+        <div className="project-index">
+            <h2>A collection of Projects built for Pursuit.</h2>
+        </div>
+    )
+}
